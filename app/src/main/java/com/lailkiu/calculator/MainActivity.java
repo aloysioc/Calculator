a92bc78cf8ea695213d2a3a9d8f8232f5e08ae13
LaryMiranda <la.ilkiu@gmail.com>
2015-09-21 23:54:22 -0300

    MainActivity code updates

--- app/src/main/java/com/lailkiu/calculator/MainActivity.java
+++ app/src/main/java/com/lailkiu/calculator/MainActivity.java
@@ -5,145 +5,123 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Switch;
 import android.widget.TextView;
-
-import com.lailkiu.calculatorv2.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static String txDisplay = "";
-    public static String txMemory = "";
-    public static String txOperator = "";
+    private String m_Display = "";
+    private String m_Memory = "";
+    private String m_Operator = "";
 
-    public static boolean okReset = true;
-    public static boolean okZeroLeft = true;
-    public static boolean okDecimal = true;
+    private boolean okReset = true;
+    private boolean okZeroLeft = true;
+    private boolean okDecimal = true;
 
-    public static double nCalc00 = 0.0;
-    public static double nCalc01 = 0.0;
-    public static double nCalcRs = 0.0;
+    private double nCalc00 = 0.0;
+    private double nCalc01 = 0.0;
+    private double nCalcRs = 0.0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView vDisplay = (TextView)findViewById(R.id.vDisplay);
-        TextView vMemory = (TextView)findViewById(R.id.vMemory);
-        TextView vOperator = (TextView)findViewById(R.id.vOperator);
-
-        vDisplay.setText(txDisplay);
-        vMemory.setText(txMemory);
-        vOperator.setText(txOperator);
-
+        setScreen();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
+    public void setScreen() {
+        TextView vDisplay = (TextView)findViewById(R.id.Display);
+        TextView vMemory = (TextView)findViewById(R.id.Memory);
+        TextView vOperator = (TextView)findViewById(R.id.Operator);
+
+        vDisplay.setText(m_Display);
+        vMemory.setText(m_Memory);
+        vOperator.setText(m_Operator);
+    }
 
     public void buttonClick(View view) {
+        int click = view.getId();
 
-        TextView vDisplay = (TextView)findViewById(R.id.vDisplay);
-        TextView vMemory = (TextView)findViewById(R.id.vMemory);
-        TextView vOperator = (TextView)findViewById(R.id.vOperator);
-
-        String strClick = view.toString();
-
-        if (strClick.contains("id/bt00")) { addNumber("0"); }
-        else if (strClick.contains("id/bt01")) { addNumber("1"); }
-        else if (strClick.contains("id/bt02")) { addNumber("2"); }
-        else if (strClick.contains("id/bt03")) { addNumber("3"); }
-        else if (strClick.contains("id/bt04")) { addNumber("4"); }
-        else if (strClick.contains("id/bt05")) { addNumber("5"); }
-        else if (strClick.contains("id/bt06")) { addNumber("6"); }
-        else if (strClick.contains("id/bt07")) { addNumber("7"); }
-        else if (strClick.contains("id/bt08")) { addNumber("8"); }
-        else if (strClick.contains("id/bt09")) { addNumber("9"); }
-        else if (strClick.contains("id/btDec")) { addNumber("."); }
-        else if (strClick.contains("id/btRes")) { addOperator("="); }
-        else if (strClick.contains("id/btSum")) { addOperator("+"); }
-        else if (strClick.contains("id/btSub")) { addOperator("-"); }
-        else if (strClick.contains("id/btMul")) { addOperator("*"); }
-        else if (strClick.contains("id/btDiv")) { addOperator("/"); }
-        else if (strClick.contains("id/btUnd")) {
-
-            if (txDisplay.length() > 0) {
-                if (txDisplay.substring(txDisplay.length()-1,txDisplay.length()).equals(".")) okDecimal = true;
-                if (txDisplay.length() == 1) okZeroLeft = true;
-                txDisplay = txDisplay.substring(0,txDisplay.length()-1);
-            }
-
+        switch (click) {
+            case R.id.bt00: addNumber("0"); break;
+            case R.id.bt01: addNumber("1"); break;
+            case R.id.bt02: addNumber("2"); break;
+            case R.id.bt03: addNumber("3"); break;
+            case R.id.bt04: addNumber("4"); break;
+            case R.id.bt05: addNumber("5"); break;
+            case R.id.bt06: addNumber("6"); break;
+            case R.id.bt07: addNumber("7"); break;
+            case R.id.bt08: addNumber("8"); break;
+            case R.id.bt09: addNumber("9"); break;
+            case R.id.btDec: addNumber("."); break;
+            case R.id.btSum: addOperator("+"); break;
+            case R.id.btSub: addOperator("-"); break;
+            case R.id.btMul: addOperator("*"); break;
+            case R.id.btDiv: addOperator("/"); break;
+            case R.id.btResult: addOperator("="); break;
+            case R.id.btBack:
+                if (m_Display.length() > 0) {
+                    if (m_Display.substring(m_Display.length()-1, m_Display.length()).equals(".")) okDecimal = true;
+                    if (m_Display.length() == 1) okZeroLeft = true;
+                    m_Display = m_Display.substring(0, m_Display.length()-1);
+                }
+                break;
+            case R.id.btReset:
+                okReset = true;
+                m_Display = "";
+                m_Memory = "";
+                m_Operator = "";
+                nCalc00 = 0.0;
+                nCalc01 = 0.0;
+                nCalcRs = 0.0;
+                break;
         }
-        else if (strClick.contains("id/btClr")) {
-
-            okReset = true;
-            txDisplay = "";
-            txMemory = "";
-            txOperator = "";
-            nCalc00 = 0.0;
-            nCalc01 = 0.0;
-            nCalcRs = 0.0;
-
-        }
-
-        vDisplay.setText(txDisplay);
-        vMemory.setText(txMemory);
-        vOperator.setText(txOperator);
-
+        setScreen();
     }
 
 
     public void addNumber(String txAdd) {
 
         if (okReset) {
-
-            txDisplay = "";
+            m_Display = "";
             okReset = false;
             okZeroLeft = true;
             okDecimal = true;
-
         }
-
 
         switch (txAdd) {
             case "0":
-
-                if (okZeroLeft == false) txDisplay += "0";
+                if (okZeroLeft == false) m_Display += "0";
                 break;
-
             case ".":
-
-                if (okDecimal) { txDisplay+= "."; okZeroLeft = false; okDecimal = false; }
+                if (okDecimal) {
+                    if (m_Display.length() == 0){ m_Display += "0."; }
+                    else { m_Display += "."; }
+                    okZeroLeft = false;
+                    okDecimal = false;
+                }
                 break;
-
             default:
-
-                txDisplay+= txAdd;
+                m_Display += txAdd;
                 okZeroLeft = false;
                 break;
-
         }
-
     }
 
 
@@ -151,57 +129,43 @@ public class MainActivity extends AppCompatActivity {
 
         if (okReset == false) {
 
-            if (txDisplay.length() > 0) {
+            if (m_Display.length() > 0) {
 
-                if (txOperator.equals("=") || txOperator.equals("")){
-                    nCalcRs = Double.parseDouble("0" + txDisplay);
+                if (m_Operator.equals("=") || m_Operator.equals("")){
+                    nCalcRs = Double.parseDouble("0" + m_Display);
                     nCalc00 = nCalcRs;
                 } else {
-                    nCalc01 = Double.parseDouble("0" + txDisplay);
+                    nCalc01 = Double.parseDouble("0" + m_Display);
                 }
 
                 nCalc00 = nCalcRs;
 
-                switch (txOperator) {
+                switch (m_Operator) {
                     case "+":
-
                         nCalcRs = nCalc00 + nCalc01;
-                        txMemory= nCalc00 + " + " + nCalc01 + " = " + nCalcRs + "\n" + txMemory;
+                        m_Memory = nCalc00 + " + " + nCalc01 + " = " + nCalcRs + "\n" + m_Memory;
                         break;
-
                     case "-":
-
                         nCalcRs = nCalc00 - nCalc01;
-                        txMemory= nCalc00 + " - " + nCalc01 + " = " + nCalcRs + "\n" + txMemory;
+                        m_Memory = nCalc00 + " - " + nCalc01 + " = " + nCalcRs + "\n" + m_Memory;
                         break;
-
                     case "*":
-
                         nCalcRs = nCalc00 * nCalc01;
-                        txMemory= nCalc00 + " * " + nCalc01 + " = " + nCalcRs + "\n" + txMemory;
+                        m_Memory = nCalc00 + " * " + nCalc01 + " = " + nCalcRs + "\n" + m_Memory;
                         break;
-
                     case "/":
-
                         if (nCalc01 != 0) {
                             nCalcRs = nCalc00 / nCalc01;
                         } else {
                             nCalcRs = 999999999999.;
                         }
-                        txMemory= nCalc00 + " / " + nCalc01 + " = " + nCalcRs + "\n" + txMemory;
+                        m_Memory = nCalc00 + " / " + nCalc01 + " = " + nCalcRs + "\n" + m_Memory;
                         break;
                 }
-
             }
-
         }
-
-        txDisplay = String.valueOf(nCalcRs);
-
+        m_Display = String.valueOf(nCalcRs);
         okReset = true;
-        txOperator = refOp;
-
+        m_Operator = refOp;
     }
-
-
 }
