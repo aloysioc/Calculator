cdaed69a61d25fceb2982ca8a0fe21d3c1f29bb2
LaryMiranda <la.ilkiu@gmail.com>
2015-09-25 00:30:34 -0300

    savedInstanceState applied to MainActivity

--- app/src/main/java/com/lailkiu/calculator/MainActivity.java
+++ app/src/main/java/com/lailkiu/calculator/MainActivity.java
@@ -5,27 +5,46 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Switch;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String m_Display = "";
-    private String m_Memory = "";
-    private String m_Operator = "";
+    private String mDisplay = "";
+    private String mMemory = "";
+    private String mOperator = "";
+    private boolean mOkReset = true;
+    private boolean mOkZeroLeft = true;
+    private boolean mOkDecimal = true;
+    private double mCalc00 = 0.0;
+    private double mCalc01 = 0.0;
+    private double mCalcRs = 0.0;
 
-    private boolean okReset = true;
-    private boolean okZeroLeft = true;
-    private boolean okDecimal = true;
-
-    private double nCalc00 = 0.0;
-    private double nCalc01 = 0.0;
-    private double nCalcRs = 0.0;
+    static final String STATE_DISPLAY = "DISPLAY";
+    static final String STATE_MEMORY = "MEMORY";
+    static final String STATE_OPERATOR = "+";
+    static final String STATE_RESET = "true";
+    static final String STATE_ZERO_LEFT = "true";
+    static final String STATE_DECIMAL = "true";
+    static final String STATE_CALC00 = "0.0";
+    static final String STATE_CALC01 = "0.0";
+    static final String STATE_CALCRS = "0.0";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState != null) {
+            mDisplay = savedInstanceState.getString(STATE_DISPLAY);
+            mMemory = savedInstanceState.getString(STATE_MEMORY);
+            mOperator = savedInstanceState.getString(STATE_OPERATOR);
+            mOkReset = savedInstanceState.getBoolean(STATE_RESET);
+            mOkZeroLeft = savedInstanceState.getBoolean(STATE_ZERO_LEFT);
+            mOkDecimal = savedInstanceState.getBoolean(STATE_DECIMAL);
+            mCalc00 = savedInstanceState.getDouble(STATE_CALC00);
+            mCalc01 = savedInstanceState.getDouble(STATE_CALC01);
+            mCalcRs = savedInstanceState.getDouble(STATE_CALCRS);
+        }
 
         setScreen();
     }
@@ -45,14 +64,30 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState) {
+        // Saving user's current state
+        savedInstanceState.putString(STATE_DISPLAY, mDisplay);
+        savedInstanceState.putString(STATE_MEMORY, mMemory);
+        savedInstanceState.putString(STATE_OPERATOR, mOperator);
+        savedInstanceState.putBoolean(STATE_RESET, mOkReset);
+        savedInstanceState.putBoolean(STATE_ZERO_LEFT, mOkZeroLeft);
+        savedInstanceState.putBoolean(STATE_DECIMAL, mOkDecimal);
+        savedInstanceState.putDouble(STATE_CALC00, mCalc00);
+        savedInstanceState.putDouble(STATE_CALC01, mCalc01);
+        savedInstanceState.putDouble(STATE_CALCRS, mCalcRs);
+        //Saving view's current hierarchy state
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
     public void setScreen() {
         TextView vDisplay = (TextView)findViewById(R.id.Display);
         TextView vMemory = (TextView)findViewById(R.id.Memory);
         TextView vOperator = (TextView)findViewById(R.id.Operator);
 
-        vDisplay.setText(m_Display);
-        vMemory.setText(m_Memory);
-        vOperator.setText(m_Operator);
+        vDisplay.setText(mDisplay);
+        vMemory.setText(mMemory);
+        vOperator.setText(mOperator);
     }
 
     public void buttonClick(View view) {
@@ -76,20 +111,20 @@ public class MainActivity extends AppCompatActivity {
             case R.id.btDiv: addOperator("/"); break;
             case R.id.btResult: addOperator("="); break;
             case R.id.btBack:
-                if (m_Display.length() > 0) {
-                    if (m_Display.substring(m_Display.length()-1, m_Display.length()).equals(".")) okDecimal = true;
-                    if (m_Display.length() == 1) okZeroLeft = true;
-                    m_Display = m_Display.substring(0, m_Display.length()-1);
+                if (mDisplay.length() > 0) {
+                    if (mDisplay.substring(mDisplay.length()-1, mDisplay.length()).equals(".")) mOkDecimal = true;
+                    if (mDisplay.length() == 1) mOkZeroLeft = true;
+                    mDisplay = mDisplay.substring(0, mDisplay.length()-1);
                 }
                 break;
             case R.id.btReset:
-                okReset = true;
-                m_Display = "";
-                m_Memory = "";
-                m_Operator = "";
-                nCalc00 = 0.0;
-                nCalc01 = 0.0;
-                nCalcRs = 0.0;
+                mOkReset = true;
+                mDisplay = "";
+                mMemory = "";
+                mOperator = "";
+                mCalc00 = 0.0;
+                mCalc01 = 0.0;
+                mCalcRs = 0.0;
                 break;
         }
         setScreen();
@@ -98,28 +133,28 @@ public class MainActivity extends AppCompatActivity {
 
     public void addNumber(String txAdd) {
 
-        if (okReset) {
-            m_Display = "";
-            okReset = false;
-            okZeroLeft = true;
-            okDecimal = true;
+        if (mOkReset) {
+            mDisplay = "";
+            mOkReset = false;
+            mOkZeroLeft = true;
+            mOkDecimal = true;
         }
 
         switch (txAdd) {
             case "0":
-                if (okZeroLeft == false) m_Display += "0";
+                if (mOkZeroLeft == false) mDisplay += "0";
                 break;
             case ".":
-                if (okDecimal) {
-                    if (m_Display.length() == 0){ m_Display += "0."; }
-                    else { m_Display += "."; }
-                    okZeroLeft = false;
-                    okDecimal = false;
+                if (mOkDecimal) {
+                    if (mDisplay.length() == 0){ mDisplay += "0."; }
+                    else { mDisplay += "."; }
+                    mOkZeroLeft = false;
+                    mOkDecimal = false;
                 }
                 break;
             default:
-                m_Display += txAdd;
-                okZeroLeft = false;
+                mDisplay += txAdd;
+                mOkZeroLeft = false;
                 break;
         }
     }
@@ -127,45 +162,45 @@ public class MainActivity extends AppCompatActivity {
 
     public void addOperator(String refOp) {
 
-        if (okReset == false) {
+        if (mOkReset == false) {
 
-            if (m_Display.length() > 0) {
+            if (mDisplay.length() > 0) {
 
-                if (m_Operator.equals("=") || m_Operator.equals("")){
-                    nCalcRs = Double.parseDouble("0" + m_Display);
-                    nCalc00 = nCalcRs;
+                if (mOperator.equals("=") || mOperator.equals("")){
+                    mCalcRs = Double.parseDouble("0" + mDisplay);
+                    mCalc00 = mCalcRs;
                 } else {
-                    nCalc01 = Double.parseDouble("0" + m_Display);
+                    mCalc01 = Double.parseDouble("0" + mDisplay);
                 }
 
-                nCalc00 = nCalcRs;
+                mCalc00 = mCalcRs;
 
-                switch (m_Operator) {
+                switch (mOperator) {
                     case "+":
-                        nCalcRs = nCalc00 + nCalc01;
-                        m_Memory = nCalc00 + " + " + nCalc01 + " = " + nCalcRs + "\n" + m_Memory;
+                        mCalcRs = mCalc00 + mCalc01;
+                        mMemory = mCalc00 + " + " + mCalc01 + " = " + mCalcRs + "\n" + mMemory;
                         break;
                     case "-":
-                        nCalcRs = nCalc00 - nCalc01;
-                        m_Memory = nCalc00 + " - " + nCalc01 + " = " + nCalcRs + "\n" + m_Memory;
+                        mCalcRs = mCalc00 - mCalc01;
+                        mMemory = mCalc00 + " - " + mCalc01 + " = " + mCalcRs + "\n" + mMemory;
                         break;
                     case "*":
-                        nCalcRs = nCalc00 * nCalc01;
-                        m_Memory = nCalc00 + " * " + nCalc01 + " = " + nCalcRs + "\n" + m_Memory;
+                        mCalcRs = mCalc00 * mCalc01;
+                        mMemory = mCalc00 + " * " + mCalc01 + " = " + mCalcRs + "\n" + mMemory;
                         break;
                     case "/":
-                        if (nCalc01 != 0) {
-                            nCalcRs = nCalc00 / nCalc01;
+                        if (mCalc01 != 0) {
+                            mCalcRs = mCalc00 / mCalc01;
                         } else {
-                            nCalcRs = 999999999999.;
+                            mCalcRs = 999999999999.;
                         }
-                        m_Memory = nCalc00 + " / " + nCalc01 + " = " + nCalcRs + "\n" + m_Memory;
+                        mMemory = mCalc00 + " / " + mCalc01 + " = " + mCalcRs + "\n" + mMemory;
                         break;
                 }
             }
         }
-        m_Display = String.valueOf(nCalcRs);
-        okReset = true;
-        m_Operator = refOp;
+        mDisplay = String.valueOf(mCalcRs);
+        mOkReset = true;
+        mOperator = refOp;
     }
 }
